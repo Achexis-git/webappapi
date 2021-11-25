6c9a1ca8577762298d36bb79e40fed84937fe0ef
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-11-25 17:56:31 +0100

    add source files

---
+++ src/main/java/com/openclassrooms/webappapi/WebappapiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.webappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappapiApplication.class, args);
+	}
+
+}
