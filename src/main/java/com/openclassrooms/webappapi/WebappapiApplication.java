f6919781288302cf1e7ea36c4437b4a103935421
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-11-30 19:24:57 +0100

    hellow world

--- src/main/java/com/openclassrooms/webappapi/WebappapiApplication.java
+++ src/main/java/com/openclassrooms/webappapi/WebappapiApplication.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.webappapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappapiApplication {
+public class WebappapiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World !");
+		
 	}
 
 }
