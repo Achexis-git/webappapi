5be689d37569f5b8595e2b469a12340753d591be
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-12-01 15:35:19 +0100

    minor change

--- src/main/java/com/openclassrooms/webappapi/WebappapiApplication.java
+++ src/main/java/com/openclassrooms/webappapi/WebappapiApplication.java
@@ -1,13 +1,17 @@
 package com.openclassrooms.webappapi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebappapiApplication implements CommandLineRunner{
+	private static final Logger logger = LogManager.getLogger(WebappapiApplication.class);
 
 	public static void main(String[] args) {
+		logger.debug("Test log4j");
 		SpringApplication.run(WebappapiApplication.class, args);
 	}
 
