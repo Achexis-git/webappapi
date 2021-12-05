3ebcc9f537a002fda50d6482875443f887f57a35
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-12-05 17:07:12 +0100

    Can read the whole file

--- src/main/java/com/openclassrooms/webappapi/repository/JsonRepository.java
+++ src/main/java/com/openclassrooms/webappapi/repository/JsonRepository.java
@@ -3,6 +3,10 @@ package com.openclassrooms.webappapi.repository;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,6 +19,8 @@ import org.springframework.stereotype.Repository;
 import com.openclassrooms.webappapi.WebappapiApplication;
 import com.openclassrooms.webappapi.model.FireStation;
 import com.openclassrooms.webappapi.model.FireStations;
+import com.openclassrooms.webappapi.model.MedicalRecord;
+import com.openclassrooms.webappapi.model.MedicalRecords;
 import com.openclassrooms.webappapi.model.Person;
 import com.openclassrooms.webappapi.model.Persons;
 
@@ -25,23 +31,30 @@ public class JsonRepository {
 
 	private static String jsonFilepath = System.getProperty("user.dir") + "/src/main/resources/data.json";
 
-	private static Persons persons = new Persons();
-	private static FireStations fireStations = new FireStations();
+	private static Persons persons               = new Persons();
+	private static FireStations fireStations     = new FireStations();
+	private static MedicalRecords medicalRecords = new MedicalRecords();
 
 	public JsonRepository() {
 
 	}
 
 	public Persons getAllPersons() {
-		// read json & update persons & fireStations
+		// read json & update persons & fireStations & medicalRecords
 		readJsonPerson();
 		return persons;
 	}
 
 	public FireStations getAllFireStations() {
-		// read json & update persons & fireStations
+		// read json & update persons & fireStations & medicalRecords
 		readJsonPerson();
 		return fireStations;
+	}
+
+	public MedicalRecords getAllMedicalRecords() {
+		// read json & update persons & fireStations & medicalRecords
+		readJsonPerson();
+		return medicalRecords;
 	}
 
 	// @SuppressWarnings("unchecked")
@@ -64,12 +77,14 @@ public class JsonRepository {
 			// Get "persons" of the JSONObject and put it into an array
 			JSONArray fireStationList = (JSONArray) objJson.get("firestations");
 			// Get "persons" of the JSONObject and put it into an array
-			JSONArray medicalRecordList = (JSONArray) objJson.get("mediaclrecords");
+			JSONArray medicalRecordList = (JSONArray) objJson.get("medicalrecords");
 
 			logger.info("Arrayed the json");
 			logger.debug("First element of the list : " + personList.get(0).toString());
 
-			// Browse personList and add each person to the object persons
+			//TODO: Dois y avoir moyen de faire ça de façon plus smart
+			
+			// Browse personList and add each person to persons
 			Person pers;
 			for (int i = 0; i < personList.size(); i++) {
 				objJson = (JSONObject) personList.get(i);
@@ -82,7 +97,7 @@ public class JsonRepository {
 				// décomposer
 			}
 
-			// Browse personList and add each person to the object persons
+			// Browse fireStationList and add each fire station to fireStaions
 			FireStation fs;
 			for (int i = 0; i < fireStationList.size(); i++) {
 				objJson = (JSONObject) fireStationList.get(i);
@@ -91,18 +106,18 @@ public class JsonRepository {
 				logger.trace("Cast the fire station");
 				fireStations.addFireStation(fs);
 				logger.trace("Add the fire station");
-				// listPerson.addPerson(parsePersonObject((JSONObject) personList.get(i))); // à
-				// décomposer
 			}
-			/*
-			 * // Browse personList and add each person to the object persons Person pers;
-			 * for (int i = 0; i < personList.size(); i++) { objJson = (JSONObject)
-			 * personList.get(i); logger.trace("Get the person " + i); pers =
-			 * parsePersonObject(objJson); logger.trace("Cast the person");
-			 * persons.addPerson(pers); logger.trace("Add the person"); //
-			 * listPerson.addPerson(parsePersonObject((JSONObject) personList.get(i))); // à
-			 * // décomposer }
-			 */
+
+			// Browse personList and add each person to the object persons Person pers;
+			MedicalRecord mr;
+			for (int i = 0; i < medicalRecordList.size(); i++) {
+				objJson = (JSONObject) medicalRecordList.get(i);
+				logger.trace("Get the medical record " + i);
+				mr = parseMedicalRecordObject(objJson);
+				logger.trace("Cast the medical record");
+				medicalRecords.addMedicalRecord(mr);
+				logger.trace("Add the medical record"); //
+			}
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -135,5 +150,34 @@ public class JsonRepository {
 		String station = (String) fireStation.get("station");
 
 		return new FireStation(address, station);
+	}
+
+	private static MedicalRecord parseMedicalRecordObject(JSONObject medicalRecord) {
+		// Read the JSONObject and return a MedicalRecord with the right attributes
+
+		String firstName = (String) medicalRecord.get("firstName");
+		String lastName  = (String) medicalRecord.get("lastName");
+		String birthdate = (String) medicalRecord.get("birthdate");
+		
+		// TODO: Ajouter les 2 attributs manquants
+		Object mObj = medicalRecord.get("medications");
+		logger.trace("Get the medication : " + mObj);
+		List<String> medication = toList((JSONArray) mObj);
+		logger.trace("Medication correctly mapped : " + medication);
+		
+		Object aObj = medicalRecord.get("allergies");
+		List<String> allergies = toList((JSONArray) aObj);
+
+		return new MedicalRecord(firstName, lastName, birthdate, medication, allergies);
+	}
+	
+	private static List<String> toList(JSONArray jsonArr) {
+		List<String> list = new ArrayList<String>();
+		for(int i=0; i<jsonArr.size(); i++) {
+			list.add((String) jsonArr.get(i));
+			logger.trace("Ajoute élément : " + (String) jsonArr.get(i));
+		}
+		
+		return list;
 	}
 }
