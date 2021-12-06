b009a1cc6094c2696bf300235fd08df29fcad1c4
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-12-06 16:10:49 +0100

    Advancing URL 4

---
+++ src/main/java/com/openclassrooms/webappapi/model/PersonPosologie.java
@@ -0,0 +1,73 @@
+package com.openclassrooms.webappapi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonPosologie {
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private int age;
+	private List<String> medication;
+	private List<String> allergies;
+
+	public PersonPosologie() {
+		super();
+	}
+
+	public PersonPosologie(int id, String firstName, String lastName, String phone) {
+		super();
+		this.firstName = firstName;
+		this.lastName  = lastName;
+		this.phone     = phone;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setMedication(List<String> medication) {
+		this.medication = medication;
+	}
+	
+	public void setAllergies(List<String> allergies) {
+		this.allergies = allergies;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public List<String> getMedication() {
+		return new ArrayList<String>(medication);
+	}
+	
+	public List<String> getAllergies() {
+		return new ArrayList<String>(allergies);
+	}
+}
