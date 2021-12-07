5290da68dfbe2d7a28083f209967fbc76722b41b
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-12-07 02:06:34 +0100

    Finished URL 4

---
+++ src/main/java/com/openclassrooms/webappapi/model/InhabitantFire.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.webappapi.model;
+
+public class InhabitantFire {
+	private int stationNumber;
+	private Home home;
+
+	public void setStationNumber(int stationNumber) {
+		this.stationNumber = stationNumber;
+	}
+
+	public void setHome(Home home) {
+		this.home = new Home();
+		this.home.setAddress(home.getAddress());
+		this.home.setHomeInhabitantList(home.getHomeInhabitantList());
+	}
+
+	public int getStationNumber() {
+		return stationNumber;
+	}
+	
+	public Home getHome() {
+		Home h = new Home();
+		h.setAddress(home.getAddress());
+		h.setHomeInhabitantList(home.getHomeInhabitantList());
+		return h;
+	}
+}
